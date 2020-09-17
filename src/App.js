bcd5e329ede0b5fbba7b75a4d7e76d476d08173f
Awetb93 <58653271+Awetb93@users.noreply.github.com>
2020-09-17 16:13:13 -0400

    newfiles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./components/header/index";
 import Headline from "./components/headline/index";
+import { fetch } from "./redux/index";
+import { useDispatch, useSelector } from "react-redux";
 import "./app.scss";
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetch());
+  }, [dispatch]);
+  const list = useSelector(state => state);
+  console.log(list);
   const tempArr = [
     {
       fname: "Awet",
@@ -12,6 +20,7 @@ function App() {
       onlineStatus: true,
     },
   ];
+
   return (
     <div className="App">
       <Header />
---
+++ src/redux/index.js
@@ -0,0 +1,21 @@
+import {
+  createSlice,
+  configureStore,
+  createAsyncThunk,
+} from "@reduxjs/toolkit";
+import Axios from "axios";
+export const fetch = createAsyncThunk("fetch/Posts", async () => {
+  const posts = await Axios.get("https://jsonplaceholder.typicode.com/posts");
+  return posts.data;
+});
+const postSlice = createSlice({
+  name: "Posts",
+  initialState: {},
+  extraReducers: {
+    [fetch.fulfilled]: (state, { payload }) => {
+      state.posts = payload;
+    },
+  },
+});
+
+export const store = configureStore({ reducer: postSlice.reducer });
